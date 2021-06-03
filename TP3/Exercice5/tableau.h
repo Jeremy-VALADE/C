7234f2bb3cc8b86710705f2a4f5327cbb0a277be
Jeremy-VALADE <valade.jeremy@yahoo.com>
2021-06-03 12:29:23 +0200

    commit

--- TP3/Exercice5/tableau.h
+++
@@ -1,8 +0,0 @@
-/* Fonction qui créait des tableaux aléatoire */
-int* creationTableauAleatoire(int taille);
-
-/* Fonction qui effectue le tri à bulle */
-void tri_a_bulle(int tableau[], int taille);
-
-/* Fonction qui affiche un tableau */ 
-void afficherTableau(int tableau[], int taille);
--- TP6/fonctions.h
+++
@@ -1,4 +0,0 @@
-int fread_board(const char* file, Board board);
-
-/* Fonction qui résout un sudoku à une solution */
-int solveSudoku(Board b, int row, int col);
